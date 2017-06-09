b024997d238db23877b48d78426eba38516eda42
Curso <alberto.ie2cmfp@gmail.com>
2017-06-09 13:26:11 +0200

    constantes.properties

---
+++ src/main/java/com/ipartek/formacion/controller/CursoController.java
@@ -0,0 +1,134 @@
+/**
+ * 
+ */
+package com.ipartek.formacion.controller;
+
+/**
+ * @author Alberto Fernandez
+ *
+ */
+import java.io.File;
+import java.io.IOException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletContext;
+import javax.validation.Valid;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+import com.ipartek.formacion.controller.validator.CursoValidator;
+import com.ipartek.formacion.dbms.persistence.Curso;
+import com.ipartek.formacion.service.interfaces.CursoService;
+
+@Controller
+@RequestMapping(value = "/cursos")
+public class CursoController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CursoController.class);
+	ModelAndView mav = null;
+	
+	@Autowired
+	private ServletContext servletContext;
+	
+	@Autowired
+	private CursoService cS;
+	
+	@Resource(name = "cursoValidator") // == @Autowired@Qualifier("cursoValidator")
+	CursoValidator validator;
+	
+	@InitBinder("curso")
+	public void initBinder(WebDataBinder binder, Locale locale) {
+		binder.setValidator(validator);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setLenient(true);
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, true));
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getAll(){
+		logger.info("METHOD CONTROLLER: getAll()");
+		mav= new ModelAndView("cursos");
+		List<Curso> cursos = cS.getAll();
+		mav.addObject("listadocursos", cursos);
+		return mav;
+	}
+	
+	@RequestMapping(value="/{id}")
+	public ModelAndView getById(@PathVariable("id") int id){
+		logger.info("METHOD CONTROLLER: getById() -- PARAMS:  " + String.valueOf(id));
+		mav= new ModelAndView("curso");
+		mav.addObject("curso",cS.getById(id));
+		return mav;
+	}
+	
+	@RequestMapping(value="/deleteCurso/{id}")
+	public String delete(@PathVariable("id") int id, Model model){
+		logger.info("METHOD CONTROLLER: deleteBarrio()");
+		cS.delete(id);
+		return "redirect:/barrios";
+	}
+	
+	@RequestMapping(value="/addCurso")
+	public String addCurso(Model model){
+		logger.info("METHOD CONTROLLER: addCurso() -- PARAM:  new");
+		model.addAttribute("curso", new Curso());
+		return "curso";
+	}
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String saveCurso(@ModelAttribute("curso")@Valid Curso curso,BindingResult bindingResult, Model model,
+			RedirectAttributes redirectMap) throws IOException {
+		
+		logger.info("METHOD CONTROLLER: saveCurso() -- PARAMS:  " + curso.toString());
+		String destino = "";
+		String txt="";
+		if (bindingResult.hasErrors()) {
+			logger.info("METHOD CONTROLLER: saveCurso() -- ERRORS: " + bindingResult.hasErrors());
+			destino = "curso";
+			txt = "Los datos de formulario contienen errores";
+		}else{
+			destino = "redirect:/cursos";
+			if(cS.getById(curso.getCodigo()) != null){
+				try {
+					logger.info(curso.toString());
+					cS.update(curso);
+					txt = "El curso se ha actualizado correctamente.";
+				} catch (Exception e) {
+					logger.info("Se ha lanzadado una excepcion update. " + e.toString());
+					txt = "Ha habido problemas en la actualización.";
+				}
+			}else{
+				try {
+					logger.info("CREATE PARAMS:  " + curso.toString());
+					cS.create(curso);
+					txt = "El curso se ha creado correctamente.";
+				} catch (Exception e) {
+					logger.info("Se ha lanzadado una excepcion create. " + e.toString());
+					txt = "Ha habido problemas al crear.";
+				}
+			}
+		}
+		return destino;	
+	}	
+}
--- src/main/java/com/ipartek/formacion/controller/HomeController.java
+++ src/main/java/com/ipartek/formacion/controller/HomeController.java
@@ -8,11 +8,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,6 +33,7 @@ import com.ipartek.formacion.service.interfaces.CursoService;
  */
 
 @Controller
+@RequestMapping(value = "/")
 public class HomeController {
 
 	@Autowired
@@ -32,7 +41,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -46,7 +55,44 @@ public class HomeController {
 		model.addAttribute("cursos", cursos);
 		
 		return "home";
-}
+	}	
+	
+	@RequestMapping(value= "login")
+		public String loginPage(){
+			return "login";
+	}
+	
+	@RequestMapping(value= "/Access_Denied")
+		public String accesoDenegado(ModelMap model){
+			model.addAttribute("model", getPrincipal());
+			return "login";
+		}
+
+	private Object getPrincipal() {
+		String username = null;
+			Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			
+			if (principal instanceof UserDetails){
+				username = ((UserDetails) principal).getUsername();
+			}else{
+				username = principal.toString();
+			}
+			
+			return username;
+	}
+	
+	@RequestMapping(value="logout")
+		public String logout(HttpServletRequest request, HttpServletResponse response){
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			
+			if(auth != null){
+				new SecurityContextLogoutHandler().logout(request, response, auth);
+			}
+			
+			return "redirect:/login.html?logout";//se recomienda que la web logout sea la de login
+			
+		}
+	
 	
 	
 }
